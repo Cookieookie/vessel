a9d417010ca1a9eb5adc837dd05ba62d4bba2eaf
Cookie Van <cookievan03@gmail.com>
2024-11-10 22:10:11 -0600

    started product and productCategory entities. Set up a one-to-many and many-to-one relationship with category and products. Will test lombok after everything else is okay.

---
+++ spring-boot-ecommerce/src/main/java/com/vessel/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,49 @@
+package com.vessel.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryNames;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
+
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryNames() {
+        return categoryNames;
+    }
+
+    public void setCategoryNames(String categoryNames) {
+        this.categoryNames = categoryNames;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}
