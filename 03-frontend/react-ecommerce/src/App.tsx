d84745412b2c7aa9e0be3415c31a8962cfe59d44
Cookie Van <cookievan03@gmail.com>
2024-11-13 20:13:26 -0600

    fixed container error from bootstrap

--- 03-frontend/react-ecommerce/src/App.tsx
+++ 03-frontend/react-ecommerce/src/App.tsx
@@ -3,16 +3,17 @@ import { Routes, Route } from "react-router-dom"
 import { Home } from './pages/home.tsx';
 import { Store } from './pages/store.tsx';
 import { About } from './pages/about.tsx';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  return  <div class="container">
+  return  <Container> <div class="container">
     <Routes>
       <Route path="/about" element = {<About />} />
       <Route path="/home" element = { <Home /> } />
       <Route path="/store" element = {<Store />} />
     </Routes>
   </div>
-  
+  </Container>
 }
 
 export default App;
