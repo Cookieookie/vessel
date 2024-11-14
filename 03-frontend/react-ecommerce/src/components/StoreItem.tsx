b50bdfe09ef2616f5f9e4d802f7841d52235c0ed
Cookie Van <cookievan03@gmail.com>
2024-11-13 22:55:03 -0600

    added currency formatting to unitPrice

--- 03-frontend/react-ecommerce/src/components/StoreItem.tsx
+++ 03-frontend/react-ecommerce/src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency.tsx";
 
 type StoreItemProps = {
     id: number
@@ -12,11 +13,22 @@ type StoreItemProps = {
 
 export function StoreItem ({ id, sku, name, description, unitPrice, imageUrl}:StoreItemProps
 ) {
-    return <Card>
+    return (
+    
+    <Card>
         <Card.Img 
         variant="top" 
         src= {imageUrl} 
         height="200px" 
-        style={{objectFit: "cover"}} />
+        style={{objectFit: "contain"}} 
+        />
+
+        <Card.Body className="d-flex flex-column">
+            <Card.Title className = "d-flex justify-content-between align-items-baseline mb-4">
+                <span className="fs-2">{name}</span>
+                <span className="ms-2 text-muted">{ formatCurrency(unitPrice) }</span>
+            </Card.Title>
+        </Card.Body>
     </Card>
+    )
 }
